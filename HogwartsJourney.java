fed0a432b316fc9dc49fc1f157ffecd227d81a98
Alberto Figuerêdo <34289440+BunnyCoder@users.noreply.github.com>
2017-12-13 16:44:31 -0200

    Atualização 2: +História +Codigo
    Aumento da história e implementação de alguns elementos de programação orientada a objetos, ainda faltam algumas adaptações no código na situação 1 (antes estava sendo feito de outra forma).

--- HogwartsJourney.java
+++ HogwartsJourney.java
@@ -8,12 +8,29 @@ import java.util.Scanner;
  * 
  * Este é o jogo de RPG chamado Hogwarts Journey
  */
+class Escolhas{
+     void oferece() {
+         System.out.println("Como você agiria? \n"
+                   + "A) Agiria com coragem\n" 
+                   + "B) Agiria com arrogância\n"
+                   + "C) Agiria com inteligência\n"
+                   + "D) Agiria com bom humor");
+    }
+}
+
 public class HogwartsJourney {
     public static void main(String[] args) throws InterruptedException {
+        Escolhas opcao = new Escolhas();
         Scanner hp = new Scanner(System.in);
-        System.out.println("    Bem-vindo ao Hogwarts Journey!");
+        
+            //              xxxxxxx INTRODUÇÃO DO JOGO xxxxxxx  
+        
+        System.out.println("    Bem-vindo ao Hogwarts Journey!\n");
+        System.out.println("    O jogo permitirá que os jogadores passem pela experiência anterior à Hogwarts, ou seja, a vida de trouxa. \n    Ao decorrer do jogo, será avaliado as escolhas e atitudes do usuário, as quais, no final, serão levadas em consideração pelo chapéu seletor, para, finalmente, decidir em qual casa o jogodar pertencerá.\n");
+        System.out.println("Regras do jogo:\n • Procure escrever na forma culta padrão\n • Escolha somente uma das opções dadas no enunciado\n ");
         System.out.println("    Que a magia esteja com você!");
-        Thread.sleep(3500);
+        opcao.oferece();
+        Thread.sleep(7000);
         byte g;
         boolean jogar = true;
         String tstJogar;
@@ -23,14 +40,19 @@ public class HogwartsJourney {
             }
             for(g=0;g<3;g++){
             Thread.sleep(750);
-            System.out.print(". . ");
+            System.out.print(". . . ");
             }
-            System.out.println("\nVocê é uma criança órfã, vive em um orfanato com 3 irmãos..");
+            
+            //    xxxxxxx INTRODUÇÃO DA HISTÓRIA / CADASTRO DO JOGADOR xxxxxxx
+            
+            System.out.println("\nSeus pais, misteriosamente, sumiram quando você tinha 5 anos, e desde então você é uma criança órfã que vive \nem um orfanato com 3 irmãos..");
             Thread.sleep(500);
             System.out.println("Qual o seu nome?");
             String nameJog;
-            do{ 
+            do{
+                
             //Validação para o nome: não pode estar vazio nem ser menor que 3
+            
             nameJog = hp.nextLine();
             }while(nameJog.equals("") || nameJog.length()<3);
             String sexJog;
@@ -47,21 +69,40 @@ public class HogwartsJourney {
             for (g = 0; g < 10; g++) {
                 System.out.println("");
             }
+            
+            //          xxxxxxx TESTE LÓGICO xxxxxxx
+            
+            System.out.println("No orfanato, você, constantemente, sofre ataques de outros meninos. E por isso, você estava pensando em como poderia\nreagir caso essa situação volte a ocorrer.\n");
+            opcao.oferece();
+            String resp = hp.nextLine();
+            resp = resp.toUpperCase();
+            
+            //              xxxxxxx SITUAÇÃO 1 xxxxxxx
+            
             System.out.println(nameJog + "!! Onde está você?! - Grita preocupadíssima dona Emília, coordenadora do orfanato.");
             Thread.sleep(2000);
             if(sexJog.equals("feminino"))
                 System.out.println("Você estava escondida, com medo de seus irmãos de criação");
             else if(sexJog.equals("masculino"))
-                System.out.println("Você estava escondido, com medo de seus irmãos de criação");
+                System.out.println("Você estava escondido, com medo de seus irmãos de criação, de repente eles te encontram, o que você faz?");
             Thread.sleep(1500);
-            System.out.println("Eles te encontram, mas por algum motivo, você reaparece no telhado do orfanato.");
-            Thread.sleep(1000);
-            System.out.println("Qual a sua reação? \nA) Uau.. \n B) Ufa. Aaahhh!? \n C) AH! Pelo menos a vista é bonita \n D) Eu.. posso fazer isso?");
-            // esquema para o jogo: 4 opções de ação, cada uma com característica de cada casa
-            // ver o total de perguntas do desenvolvimento, if (contador da casa > 50% total) = defina a casa
+            System.out.println("A) Você vai pra cima deles! \n"
+                    + "B) 'Afastem-se de mim, pebleus!'\n"
+                    + "C)'CARAMBA! UM GRIFO!!' - Sai correndo \n"
+                    + "D) 'Gente, vocês viram o que o tio Harry fez ontem??'");
             
+            Thread.sleep(1500);
+            if(resp.equals("A"))
+                System.out.println("Você vai pra cima deles e soca um deles, mas os outros 2 te imobilizam.. e o terceiro volta para te machucar.");
             
+            else if(resp.equals("B"))
+                System.out.println("Eles ficaram com raiva.. e confusos, você também mora com eles! Mesmo assim, você não escapa da surra,\n mas não ficou nenhuma marca muito visível. Você volta pra casa, mas Dona Emília não percebe nada diferente.");
+            else if(resp.equals("C"))
+                System.out.println("Eles caíram como um patinho, e você consegue escapar! Você encontra Dona Emília e se salva, por hoje..");
+            else if(resp.equals("D"))
+                System.out.println("Eles respondem: 'Não, o quê?'\nVocê: 'Pergunta pra tia Emília!' - e corre até ela.");
             
+            System.out.println("");
             
             
             
